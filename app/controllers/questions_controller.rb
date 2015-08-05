7e5b51ed6c92e5ea6c0a597a8b1367935fc897e7
Alex Heinen <alexheinen99@gmail.com>
2015-08-04 22:15:54 -0700

    allow Julian and Alex to be 'admin' users in order for them to show up at the top of every new hive

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -10,6 +10,13 @@ class QuestionsController < ApplicationController
     if question.save
       hive = Hive.create(question_id: question.id, solo: params[:solo])
       HiveUser.create(hive_id: hive.id, user_id: @current_user.id)
+
+      ## For testing purposes, we will allow Julian and Alex to become the admin users for every hive
+      if @current_user.id != 1 && @current_user.id != 2
+        HiveUser.create(hive_id: hive.id, user_id: 1)
+        HiveUser.create(hive_id: hive.id, user_id: 2)
+      end
+
       render json: {user_id: @current_user.id}
     end
   end
