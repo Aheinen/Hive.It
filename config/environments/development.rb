6ca1b5c089b9649cb4c0033ec12f1aaf670c2d7f
Alex Heinen <alexheinen99@gmail.com>
2015-08-05 10:14:13 -0700

    configure pusher in both production and development environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,9 +38,9 @@ Rails.application.configure do
 
   require 'pusher'
 
-  Pusher.app_id = '131952'
-  Pusher.key = 'cf25b0bc9947e040c849'
-  Pusher.secret = '736374339876eb7e55fc'
+    Pusher.app_id = ENV['PUSHER_APP_ID']
+    Pusher.key = ENV['PUSHER_KEY']
+    Pusher.secret = ENV['PUSHER_SECRET']
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
